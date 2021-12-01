9889b519aed47a605ab49c657c245cd7e0a1cde7
CodeSalvageON <65372786+CodeSalvageON@users.noreply.github.com>
2021-12-01 02:33:33 +0000

    Initial commit

---
+++ index.php
@@ -0,0 +1,19 @@
+<HTML>
+  <HEAD>
+    <TITLE>UNIMATRIXZERO</TITLE>
+    
+    <LINK href="/UNI/GRAPHICS/GUI.CSS" rel="STYLESHEET">
+  </HEAD>
+  <BODY>
+    <SECTION id="GUI">
+      <BR/>
+      <CENTER id="STARTUP">
+        <H1 id="TITLE-TEXT" class="glass">UNIMATRIXZERO</H1>
+        <BR/>
+        <BR/>
+        <H2 id="CREATE-ACCOUNT-BTN" class="glass">Create Account</H2>
+        <H2 id="LOGIN-ACCOUNT-BTN" class="glass">Login</H2>
+      </CENTER>
+    </SECTION>
+  </BODY>
+</HTML>
